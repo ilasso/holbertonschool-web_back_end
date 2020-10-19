c72941b699a094ae3ed3a9ce213e9c16ef918849
ilasso <1204@holbertonschool.com>
2020-10-19 12:05:25 -0500

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""2-floor: module to define floor function"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    add: function that return floor of a float
+    """
+    return math.floor(n)
