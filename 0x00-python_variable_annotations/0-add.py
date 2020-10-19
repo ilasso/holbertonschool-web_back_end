5e0b3b9b10179c4c9cc8e7c294082cba74baec0c
ilasso <1204@holbertonschool.com>
2020-10-19 11:54:22 -0500

    Basic annotations - concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """0-add: module to define add function"""
-def add(a:float, b:float) -> float:
+
+
+def add(a: float, b: float) -> float:
     """
     add: function that sum two floats
     """
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""1-concat: module to define concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    add: function that concatenate two strings
+    """
+    return str1 + str2
