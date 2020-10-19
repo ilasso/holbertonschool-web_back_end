21aec351da283a5814f89b6af36f1f0da4bc8d24
ilasso <1204@holbertonschool.com>
2020-10-19 12:52:38 -0500

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""3-to_str: module to define to_str function"""
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    add: function that convert to string a float
+    """
+    return str(n)
