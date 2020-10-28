b19d48023d2777c60c299c365bae982881721757
ilasso <1204@holbertonschool.com>
2020-10-28 15:58:28 -0500

    t1

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Simple helper function
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ return a tuple of size two containing a start index
+        and an end index corresponding to the range of indexes
+    """
+    if page and page_size:
+        start_index: int = (page - 1) * page_size
+        end_index: int = start_index + page_size
+    return (start_index, end_index)
