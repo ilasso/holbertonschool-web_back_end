74e8b691519f1de1e4424296e1cc722abe027c7a
ilasso <1204@holbertonschool.com>
2020-11-05 17:46:36 -0500

    Request validation

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -12,6 +12,29 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+authtype = getenv('AUTH_TYPE')
+
+if authtype == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def br():
+    """
+    br
+    """
+    listpaths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                 '/api/v1/forbidden/']
+    if not auth:
+        return None
+    if not auth.require_auth(request.path, listpaths):
+        return None
+    if not auth.authorization_header(request):
+        abort(401)
+    if not auth.current_user(request):
+        abort(403)
 
 
 @app.errorhandler(404)
--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -30,7 +30,8 @@ class Auth():
         """
         authorization_header
         """
-        return None
+        if request:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
