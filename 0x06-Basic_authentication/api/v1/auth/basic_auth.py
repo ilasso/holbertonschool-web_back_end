626f33a1327bb7109d6396b814d56a2c8b6d881c
ilasso <1204@holbertonschool.com>
2020-11-05 23:37:37 -0500

    Basic - Base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,21 @@ class BasicAuth(Auth):
         if spauthheader[0] != "Basic":
             return None
         return spauthheader[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """
+        decode_base64_authorization_header
+        """
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            b64bytes = base64_authorization_header.encode('utf-8')
+            bytes = base64.b64decode(b64bytes)
+            return bytes.decode('utf-8')
+        except Exception:
+            return None
