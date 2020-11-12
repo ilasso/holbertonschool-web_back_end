e85068df579a6751e30c05b1f404564f14e32b98
ilasso <1204@holbertonschool.com>
2020-11-11 22:44:21 -0500

    errors

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -35,7 +35,7 @@ def login() -> str:
 @app_views.route('/auth_session/logout', methods=['DELETE'],
                  strict_slashes=False)
 def logout() -> str:
-    """ DELETE /auth_session/llogout
+    """ DELETE /auth_session/logout
     Return:
         - json string
     """
