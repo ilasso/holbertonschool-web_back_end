ae9abf8aed2f73184a50969574f5f1726a7a0564
ilasso <1204@holbertonschool.com>
2020-11-15 16:25:07 -0500

    Find user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base
 from user import User
@@ -12,15 +13,23 @@ from typing import TypeVar
 
 
 class DB:
-
+    """
+        class to map db tables in a python object class
+    """
     def __init__(self):
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """
+        constructor
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self):
+        """
+            create session conect
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -32,3 +41,12 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs: dict) -> object:
+        """ returns the first row found in the users table
+            as filtered by the method’s input arguments
+        """
+        obj = self._session.query(User).filter_by(**kwargs).first()
+        if not obj:
+            raise NoResultFound
+        return obj
