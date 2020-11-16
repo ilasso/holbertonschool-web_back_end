7d5571be2d75016baa1eae9adc0254432bc74866
ilasso <1204@holbertonschool.com>
2020-11-16 13:12:22 -0500

    type

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -42,7 +42,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: any) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """ returns the first row found in the users table
             as filtered by the method’s input arguments
         """
