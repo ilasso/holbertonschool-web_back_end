f9bf849bdc38626d5e7830abb7a24e11b56ad56e
ilasso <1204@holbertonschool.com>
2020-11-24 17:55:10 -0500

    More patching

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -34,3 +34,18 @@ class TestGithubOrgClient(unittest.TestCase):
             res = g_client._public_repos_url
             mock_org.assert_called_once()
             self.assertEqual(res, test_json.get("repos_url"))
+
+    @mock.patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """Testing public_repos method
+        """
+        mock_get_json.return_value = [{"name": "google"},
+                                      {"name": "abc"}]
+        with mock.patch.object(GithubOrgClient, "_public_repos_url",
+                               new_callable=PropertyMock) as mock_public:
+            mock_public.return_value = "http://testurl.com"
+            g_client = GithubOrgClient("facebook")
+            res = g_client.public_repos()
+            self.assertEqual(res, ["google", "abc"])
+            mock_get_json.assert_called_once()
+            mock_public.assert_called_once()
