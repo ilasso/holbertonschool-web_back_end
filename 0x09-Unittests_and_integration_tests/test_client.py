2963e554d1969947651eebbf7fd4ee5f6460b0d5
ilasso <1204@holbertonschool.com>
2020-11-24 20:35:39 -0500

    Parameterize

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -54,6 +54,17 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once()
             mock_public.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license, expected):
+        """Testing for test_has_license
+        """
+        g_client = GithubOrgClient("facebook")
+        res = (g_client.has_license(repo, license))
+        self.assertEqual(res, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
