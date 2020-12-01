c3f3ce332e9fd66b3b3dfcb72e240db3207886a1
ilasso <1204@holbertonschool.com>
2020-12-01 14:12:01 -0500

    poc5

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -28,3 +28,16 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes,
+                                                          int, float]:
+        """
+        method that take a key string argument and an optional
+        Callable argument named fn. This callable will be used
+        to convert the data back to the desired format
+        """
+        kvalue = self._redis.get(key)
+        value = kvalue
+        if kvalue and fn:
+            value = fn(kvalue)
+        return value
