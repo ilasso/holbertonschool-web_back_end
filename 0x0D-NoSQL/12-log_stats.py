cdd5e000a91f6226f2e1b3c2158a3a383b9d5ccf
ilasso <1204@holbertonschool.com>
2020-12-08 16:30:02 -0500

    log stats

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python3
-"""Log stats"""
+"""script that provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
 
-def log(a: dict) -> int:
-    """return log"""
+def getlog(a: dict) -> int:
+    """count log"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_logs = client.logs.nginx
     return nginx_logs.count_documents(a)
 
 
-def print_nginx_logs_stats():
+def print_resume():
     """
-    provides some stats about
-    Nginx logs stored in MongoDB
+    print resume data
     """
-    print(f"{ log({}) } logs")
+    print(f"{getlog({})} logs")
     print("Methods:")
-    print(f"\tmethod GET: { log({'method': 'GET'}) }")
-    print(f"\tmethod POST: { log({'method': 'POST'}) }")
-    print(f"\tmethod PUT: {log({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {log({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {log({'method': 'DELETE'})}")
-    print(f"{log({'method': 'GET', 'path': '/status'})} status check")
+    print(f"\tmethod GET: {getlog({'method': 'GET'}) }")
+    print(f"\tmethod POST: {getlog({'method': 'POST'}) }")
+    print(f"\tmethod PUT: {getlog({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {getlog({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {getlog({'method': 'DELETE'})}")
+    print(f"{getlog({'method': 'GET', 'path': '/status'})} status check")
 
 
 if __name__ == "__main__":
-    print_nginx_logs_stats()
-
+    print_resume()
