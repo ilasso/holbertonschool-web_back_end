300602d051ef34061bf7831d4b8a833e193dfacd
ilasso <1204@holbertonschool.com>
2020-12-09 23:26:32 -0500

    Let's create a report object

---
+++ 0x0E-ES6_basic/12-test.js
@@ -0,0 +1,12 @@
+import createReportObject from "./12-createReportObject.js"
+let lista = createReportObject (
+{
+  allEmployees: {
+     engineering: [
+          'John Doe',
+          'Guillaume Salva',
+     ],
+  },
+}	
+);
+console.log(lista)
