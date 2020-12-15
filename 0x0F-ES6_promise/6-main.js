6aeb3d4664ef110f30647fb8122acc2816825b39
ilasso <1204@holbertonschool.com>
2020-12-14 20:59:08 -0500

    Handle multiple promises

---
+++ 0x0F-ES6_promise/6-main.js
@@ -0,0 +1,5 @@
+import handleProfileSignup from './6-final-user';
+
+let a = handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg");
+//console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+console.log(a)
