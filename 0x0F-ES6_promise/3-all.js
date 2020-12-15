6b7bd92a277e935db7f3be716494c2c03e4fa788
ilasso <1204@holbertonschool.com>
2020-12-15 18:06:06 -0500

    poc

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,19 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      let body;
-      let firstname;
-      let lastname;
-      for (const response of results) {
-        if (Object.prototype.hasOwnProperty.call(response, 'body')) {
-          body = response.body;
-        }
-        if (Object.prototype.hasOwnProperty.call(response, 'firstName')) {
-          firstname = response.firstName;
-          lastname = response.lastName;
-        }
-      }
-      console.log(body, firstname, lastname);
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
