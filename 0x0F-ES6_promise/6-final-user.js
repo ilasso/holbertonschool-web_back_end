a9f9027943e991e2d50523420a65b34f3e2d3a2d
ilasso <1204@holbertonschool.com>
2020-12-15 21:08:15 -0500

    return array

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,5 +3,15 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results);
+    .then((results) => {
+      const lista = [];
+      for (const value of results) {
+        if (value.status === 'rejected') {
+          value.value = value.reason.toString().substring(0, 40);
+          delete value.reason;
+        }
+        lista.push(value);
+      }
+      return lista;
+    });
 }
