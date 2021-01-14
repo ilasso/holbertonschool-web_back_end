2d5861a174971c94747a3dc0cfa57022741fa3eb
ilasso <1204@holbertonschool.com>
2021-01-14 14:24:41 -0500

    cases

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -7,6 +7,11 @@ describe('calculateNumber', () => {
       assert.equal(calculateNumber(0.0, 0), 0);
       assert.equal(calculateNumber(1.9, 0), 2);
       assert.equal(calculateNumber(0, 1.9), 2);
+      assert.strictEqual(calculateNumber(4, 8), 12);
+    assert.strictEqual(calculateNumber(1.9, 0), 2);
+    assert.strictEqual(calculateNumber(6.1, 6.1), 12);
+    assert.strictEqual(calculateNumber(0.1, 0.2), 0);
+    assert.strictEqual(calculateNumber(0.1, 0.6), 1);
     });
     it('negative numbers', () => {
       assert.equal(calculateNumber(-1, 1), 0);
@@ -15,6 +20,8 @@ describe('calculateNumber', () => {
     it('checks arguments', () => {
       assert.equal(isNaN(calculateNumber(2.2)), true);
       assert.equal(isNaN(calculateNumber()), true);
+      assert.strictEqual(calculateNumber(-1, -3), -4);
+    assert.strictEqual(calculateNumber(-1.4, -3.6), -5);
     });
   });
   
