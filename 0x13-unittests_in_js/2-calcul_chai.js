6ed4ba9d1a25539fcd508d2a39d3d0e7689cb6f2
ilasso <1204@holbertonschool.com>
2021-01-14 16:15:57 -0500

    chai

---
+++ 0x13-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,19 @@
+function calculateNumber (type, a, b) {
+    switch (type) {
+        case 'SUM':
+            return Math.round(a) + Math.round(b);        
+            break;
+        case 'SUBTRACT':
+                return Math.round(a) - Math.round(b);
+                break;
+        case 'DIVIDE':
+            if (!Math.round(b)) return 'Error';
+            return Math.round(a) / Math.round(b);
+            break;                
+        default:
+            break;
+    }
+    
+}
+
+module.exports = calculateNumber;
