f629e190773e70f1609f7df37b8b19ac6516765f
ilasso <1204@holbertonschool.com>
2021-01-19 17:45:51 -0500

    Node Redis client publisher and subscriber

---
+++ 0x14-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,24 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', function(error) {
+    if (error)
+      console.log(`Redis client not connected to the server: ${error}`);
+  });
+  
+  client.on('ready', function(){
+      console.log('Redis client connected to the server');
+  });
+  
+
+const publishMessage = (message, time) => {
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    client.publish('holberton school channel', message);
+  }, time);
+}
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
---
+++ 0x14-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', function(error) {
+    if (error)
+      console.log(`Redis client not connected to the server: ${error}`);
+  });
+  
+  client.on('ready', function(){
+      console.log('Redis client connected to the server');
+  });
+  
+client.subscribe("holberton school channel");
+
+client.on('message', (channel, message) => {
+    console.log(message);
+    if (message === 'KILL_SERVER') {
+      client.unsubscribe(channel);
+      client.quit();
+    }
+  });
+  
+  
