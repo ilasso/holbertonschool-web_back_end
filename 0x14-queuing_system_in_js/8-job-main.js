f6a2f75a024bb18eae10ba76acf49557d21a5b3e
ilasso <1204@holbertonschool.com>
2021-01-20 13:48:59 -0500

    OK

--- 0x14-queuing_system_in_js/8-job-main.js
+++ 0x14-queuing_system_in_js/8-job-main.js
@@ -7,11 +7,7 @@ const queue = kue.createQueue();
 const list = [
     {
         phoneNumber: '4153518780',
-<<<<<<< HEAD
-        message: 'This is the code 1234 to verify your account'
-=======
     message: 'This is the code 1234 to verify your account'
->>>>>>> 1a9fbec56324279c0a8f96240d916f50dff75154
     }
 ];
 createPushNotificationsJobs(list, queue);
--- 0x14-queuing_system_in_js/8-job.js
+++ 0x14-queuing_system_in_js/8-job.js
@@ -1,23 +1,13 @@
 export default function createPushNotificationsJobs (jobs, queue)  {
     if (!(jobs instanceof Array))  throw Error('Jobs is not an array');
     jobs.forEach((j) => {
-<<<<<<< HEAD
       const job = queue.create('push_notification_code_3', j);
         job.save((err) => {
-          if (!err) console.log(`Notification job created: ${job.id}`);
-        });
-      job.on('complete', (result) => console.log(`Notification job ${job.id} completed`));
-      job.on('failed', (error) => console.log(`Notification job ${job.id} failed: ${error}`));
-      job.on('progress', (progress, data) => console.log(`Notification job ${job.id} ${progress}% complete`));
-=======
-      const job = queue.create('push_notification_code_3', j)
-        .save((err) => {
           if (!err) console.log(`Notification job created: ${job.id}`);
         });
       job.on('complete', () => console.log(`Notification job ${job.id} completed`));
       job.on('failed', (error) => console.log(`Notification job ${job.id} failed: ${error}`));
       job.on('progress', (progress) => console.log(`Notification job ${job.id} ${progress}% complete`));
->>>>>>> 1a9fbec56324279c0a8f96240d916f50dff75154
     });
   };
   
